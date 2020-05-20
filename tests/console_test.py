e1ff854fbb4101391df5132e57ff9615740234af
Artyom Kaltovich <kaltovichartyom@gmail.com>
2020-05-20 17:30:14 +0000

    add tests

--- tests/console_test.py
+++ tests/console_test.py
@@ -4,6 +4,15 @@ import pytest
 
 
 def test_run_without_parameters():
-    result = subprocess.run(["python", "IsoQuant/isoquant.py"], capture_output=True)
-    assert result.returncode == 255
-    assert result.stdout == b'Output folder was not specified\n'
+    result = subprocess.run(["python", "isoquant.py"], capture_output=True)
+    assert result.returncode == 2
+    assert b"usage" in result.stderr
+    assert b"isoquant.py: error: the following arguments are required: --data_type/-d, --genedb/-g" in result.stderr
+
+
+@pytest.mark.parametrize("option", ["-h", "--help", "--full-help"])
+def test__help(option):
+    result = subprocess.run(["python", "isoquant.py", option], capture_output=True)
+    assert result.returncode == 0
+    assert b"usage" in result.stdout
+    assert b"optional arguments:" in result.stdout
