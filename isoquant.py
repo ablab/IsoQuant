c8df04cd6a8b42cd15d8ac65a5f877c1a9f9118e
Andrey Prjibelski <andrewprzh@gmail.com>
2020-06-25 04:01:08 +0300

    no debug

--- isoquant.py
+++ isoquant.py
@@ -160,14 +160,14 @@ def create_output_dirs(args):
 
 
 def set_logger(args, logger_instnace):
-    logger_instnace.setLevel(logging.DEBUG)
+    logger_instnace.setLevel(logging.INFO)
     log_file = os.path.join(args.output, "isoquant.log")
     f = open(log_file, "w")
     f.write("CMD: " + ' '.join(sys.argv) + '\n')
     f.close()
     fh = logging.FileHandler(log_file)
     #FIXME
-    fh.setLevel(logging.DEBUG)
+    fh.setLevel(logging.INFO)
     ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(logging.INFO)
 
