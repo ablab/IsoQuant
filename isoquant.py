9dff45414280404379be1038a64ab471cfd48253
Smetanin Alexander <aleksandr_smetanin@epam.com>
2020-07-02 12:12:53 +0300

    gene db lazy conversion

--- isoquant.py
+++ isoquant.py
@@ -8,6 +8,7 @@
 
 import os
 import sys
+import json
 import logging
 import argparse
 from traceback import print_exc
@@ -221,16 +222,42 @@ def set_additional_params(args):
     args.matching_stategy = MultimapResolvingStrategy(multimap_strategies[args.matching_stategy])
 
 
+def convert_gene_db(args):
+    gtf_filename = args.genedb
+    config_dir = os.path.join(os.environ['HOME'], '.config', 'IsoQuant')
+    config_path = os.path.join(config_dir, 'db_config.json')
+
+    genedb_filename = os.path.join(args.output, os.path.splitext(os.path.basename(gtf_filename))[0] + ".db")
+    if not os.path.exists(config_path):
+        os.makedirs(config_path, exist_ok=True)
+        converted_gtfs = {}
+    else:
+        converted_gtfs = json.load(config_path)
+        gtf_mtime = converted_gtfs.get(gtf_filename, {}).get('gtf_mtime')
+        if os.path.getmtime(gtf_filename) == gtf_mtime:
+            genedb_filename = converted_gtfs.get(gtf_filename, {}).get('genedb')
+            db_mtime = converted_gtfs.get(gtf_filename, {}).get('db_mtime')
+            if os.path.exists(genedb_filename) and os.path.getmtime(genedb_filename) == db_mtime:
+                logger.info("Gene annotation file was already converted to .db format")
+                logger.info("Using gene database from " + genedb_filename)
+                return genedb_filename
+
+    logger.info("Converting gene annotation file to .db format (takes a while)...")
+    gtf2db(gtf_filename, genedb_filename)
+    converted_gtfs[gtf_filename] = {'genedb': genedb_filename,
+                                    'gtf_mtime': os.path.getmtime(gtf_filename),
+                                    'db_mtime': os.path.getmtime(genedb_filename)}
+    json.dump(converted_gtfs, config_path)
+    logger.info("Gene database written to " + genedb_filename)
+    logger.info("Provide this database next time to avoid excessive conversion")
+    return genedb_filename
+
+
 def run_pipeline(args):
     logger.info(" === IsoQuant pipeline started === ")
-    # convert GTF/GFF in needed
+    # convert GTF/GFF if needed
     if not args.run_aligner_only and not args.genedb.endswith('db'):
-        genedb_filename = args.genedb
-        args.genedb = os.path.join(args.output, os.path.splitext(os.path.basename(genedb_filename))[0] + ".db")
-        logger.info("Converting gene annotation file to .db format (takes a while)...")
-        gtf2db(genedb_filename, args.genedb)
-        logger.info("Gene database written to " + args.genedb)
-        logger.info("Provide this database next time to avoid excessive conversion")
+        args.genedb = convert_gene_db(args)
 
     # map reads if fastqs are provided
     if args.input_data.input_type == "fastq":
@@ -247,8 +274,9 @@ def run_pipeline(args):
         dataset_processor.process_all_samples(args.input_data)
     logger.info(" === IsoQuant pipeline finished === ")
 
+
 def clean_up(args):
-    #TODO
+    # TODO
     pass
 
 
