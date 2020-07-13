af3c1dc0c1a111f887958a6e605cdb5b8d1bca45
Andrey Prjibelski <andrewprzh@gmail.com>
2020-07-13 17:16:23 +0300

    use previous db

--- src/gtf2db.py
+++ src/gtf2db.py
@@ -48,6 +48,16 @@ def convert_db_to_gtf(args):
     return gtf_filename
 
 
+def find_coverted_db(converted_gtfs, gtf_filename):
+    gtf_mtime = converted_gtfs.get(gtf_filename, {}).get('gtf_mtime')
+    db_mtime = converted_gtfs.get(gtf_filename, {}).get('db_mtime')
+    db_file = converted_gtfs.get(gtf_filename, {}).get('genedb')
+    if os.path.exists(gtf_filename) and os.path.getmtime(gtf_filename) == gtf_mtime:
+        if os.path.exists(db_file) and os.path.getmtime(db_file) == db_mtime:
+            return db_file
+    return None
+
+
 def compare_stored_gtf(converted_gtfs, gtf_filename, genedb_filename):
     gtf_mtime = converted_gtfs.get(gtf_filename, {}).get('gtf_mtime')
     db_mtime = converted_gtfs.get(gtf_filename, {}).get('db_mtime')
@@ -57,6 +67,7 @@ def compare_stored_gtf(converted_gtfs, gtf_filename, genedb_filename):
 
 
 def convert_db(gtf_filename, genedb_filename, convert_fn, complete_db=False):
+    genedb_filename = os.path.abspath(genedb_filename)
     config_dir = os.path.join(os.environ['HOME'], '.config', 'IsoQuant')
     config_path = os.path.join(config_dir, 'db_config.json')
 
@@ -67,9 +78,10 @@ def convert_db(gtf_filename, genedb_filename, convert_fn, complete_db=False):
         with open(config_path, 'r') as f_in:
             converted_gtfs = json.load(f_in)
         if convert_fn == gtf2db:
-            if compare_stored_gtf(converted_gtfs, gtf_filename, genedb_filename):
-                logger.info("Gene annotation file was already converted")
-                return gtf_filename, genedb_filename
+            converted_db = find_coverted_db(converted_gtfs, gtf_filename)
+            if converted_db is not None:
+                logger.info("Gene annotation file was already converted to " + converted_db)
+                return gtf_filename, converted_db
         else:
             for converted_gtf in converted_gtfs:
                 if compare_stored_gtf(converted_gtfs, converted_gtf, genedb_filename):
