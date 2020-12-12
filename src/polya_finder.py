f8cf37ee71c907e202e4e40d243e3edb06b36c1e
Andrey Prjibelski <andrewprzh@gmail.com>
2020-12-12 18:40:02 +0300

    optimize polyA search

--- src/polya_finder.py
+++ src/polya_finder.py
@@ -178,11 +178,18 @@ class PolyAFinder:
         if len(seq) < self.window_size:
             return -1
         i = 0
+        a_count = seq[0:self.window_size].count('A')
         while i < len(seq) - self.window_size:
-            a_count = seq[i:i + self.window_size].count('A')
             if a_count >= self.polyA_count:
                 break
+            first_base_a = seq[i] == 'A'
+            new_base_a = i + self.window_size < len(seq) and seq[i + self.window_size] == 'A'
+            if first_base_a and not new_base_a:
+                a_count -= 1
+            elif not first_base_a and new_base_a:
+                a_count += 1
             i += 1
+
         if i >= len(seq) - self.window_size:
             return -1
 
