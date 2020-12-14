d732f48a3d03a26d093fd40ccc389bc45ead0087
Andrey Prjibelski <andrewprzh@gmail.com>
2020-12-14 16:06:40 +0300

    more asserts into console tests

--- tests/console_test.py
+++ tests/console_test.py
@@ -24,8 +24,9 @@ def test_clean_start():
     data_dir = os.path.join(source_dir, 'simple_data/')
     out_dir = os.path.join(source_dir, "out_full/")
     shutil.rmtree(out_dir, ignore_errors=True)
-    # FIXME
-    os.environ['HOME'] = source_dir
+    os.environ['HOME'] = source_dir # dirty hack to set $HOME for tox environment
+    sample_name = "ONT_Simulated.chr9.4M"
+
     result = subprocess.run(["python", "isoquant.py",
                              "--clean_start",
                              "-o", out_dir,
@@ -34,19 +35,28 @@ def test_clean_start():
                              "--genedb", data_dir + "chr9.4M.gtf.gz", "--complete_genedb",
                              "-r",  data_dir + "chr9.4M.fa.gz",
                              "-t", "4",
-                             "--labels", "ONT_Simulated.chr9.4M",
+                             "--labels", sample_name,
                              "--count_exons", "--sqanti_output",
                              "--read_group","file:" + data_dir + "chr9.4M.ont.sim.read_groups.tsv" + ":0:1"])
-    # TODO add asserts
+
     assert result.returncode == 0
+    sample_folder = os.path.join(out_dir, sample_name)
+    assert os.path.isdir(sample_folder)
+    resulting_files = ["bam", "exon_counts.tsv", "exon_grouped_counts.tsv", "gene_counts.tsv", "gene_grouped_counts.tsv",
+                       "intron_counts.tsv", "intron_grouped_counts.tsv", "mapped_reads.bed", "read_assignments.tsv",
+                       "SQANTI-like.tsv", "transcript_counts.tsv", "transcript_grouped_counts.tsv",
+                       "transcript_models_counts.tsv", "transcript_models.gtf", "transcript_models_reads.tsv"]
+    for f in resulting_files:
+        assert os.path.exists(os.path.join(sample_folder, sample_name + "." + f))
 
 def test_usual_start():
     source_dir = os.path.dirname(os.path.realpath(__file__))
     data_dir = os.path.join(source_dir, 'simple_data/')
     out_dir = os.path.join(source_dir, "out_usual/")
     shutil.rmtree(out_dir, ignore_errors=True)
-    # FIXME
-    os.environ['HOME'] = source_dir
+    os.environ['HOME'] = source_dir # dirty hack to set $HOME for tox environment
+    sample_name = "ONT_Simulated.chr9.4M"
+
     result = subprocess.run(["python", "isoquant.py",
                              "-o", out_dir,
                              "--data_type", "nanopore",
@@ -54,16 +64,24 @@ def test_usual_start():
                              "--genedb", data_dir + "chr9.4M.gtf.gz", "--complete_genedb",
                              "-r",  data_dir + "chr9.4M.fa.gz",
                              "-t", "4",
-                             "--labels", "ONT_Simulated.chr9.4M"])
-    # TODO add asserts
-    assert result.returncode == 0
+                             "--labels", sample_name])
 
-def test_c():
+    assert result.returncode == 0
+    sample_folder = os.path.join(out_dir, sample_name)
+    assert os.path.isdir(sample_folder)
+    resulting_files = ["gene_counts.tsv", "read_assignments.tsv", "transcript_counts.tsv",
+                       "transcript_models_counts.tsv", "transcript_models.gtf", "transcript_models_reads.tsv"]
+    for f in resulting_files:
+        assert os.path.exists(os.path.join(sample_folder, sample_name + "." + f))
+
+def test_with_bam_and_polya():
     source_dir = os.path.dirname(os.path.realpath(__file__))
     data_dir = os.path.join(source_dir, 'simple_data/')
     out_dir = os.path.join(source_dir, "out_polya/")
     shutil.rmtree(out_dir, ignore_errors=True)
     os.environ['HOME'] = source_dir
+    sample_name = "ONT_Simulated.chr9.4M.polyA"
+
     result = subprocess.run(["python", "isoquant.py",
                              "-o", out_dir,
                              "--data_type", "nanopore",
@@ -71,9 +89,16 @@ def test_c():
                              "--genedb", data_dir + "chr9.4M.gtf.gz", "--complete_genedb",
                              "-r",  data_dir + "chr9.4M.fa.gz",
                              "-t", "4",
-                             "--labels", "ONT_Simulated.chr9.4M.polyA",
-                             "--has_polya", "--sqanti_output", "--count_exons",
-                             "--read_group","file:" + data_dir + "chr9.4M.ont.sim.read_groups.tsv" + ":0:1"])
-    # TODO add asserts
+                             "--labels", sample_name,
+                             "--has_polya", "--sqanti_output", "--count_exons"])
+
     assert result.returncode == 0
+    sample_folder = os.path.join(out_dir, sample_name)
+    assert os.path.isdir(sample_folder)
+    resulting_files = ["exon_counts.tsv", "gene_counts.tsv",
+                       "intron_counts.tsv", "mapped_reads.bed", "read_assignments.tsv",
+                       "SQANTI-like.tsv", "transcript_counts.tsv",
+                       "transcript_models_counts.tsv", "transcript_models.gtf", "transcript_models_reads.tsv"]
+    for f in resulting_files:
+        assert os.path.exists(os.path.join(sample_folder, sample_name + "." + f))
 
