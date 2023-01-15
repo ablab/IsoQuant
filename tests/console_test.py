2f9fd70491f91cf8979f8adb69ec73c9d6b2f02c
Andrey Prjibelski <andrewprzh@gmail.com>
2023-01-15 17:08:18 +0200

    fix unit tests

--- tests/console_test.py
+++ tests/console_test.py
@@ -7,14 +7,15 @@ import shutil
 
 def test_run_without_parameters():
     result = subprocess.run(["python", "isoquant.py"], capture_output=True)
-    assert result.returncode == 2
-    assert b"usage" in result.stderr
-    assert b"error: the following arguments are required: --reference/-r, --data_type/-d" in result.stderr
+    assert result.returncode == 255
+    assert b"usage" in result.stdout
+    assert b"Reference genome was not provided" in result.stdout
 
 
 @pytest.mark.parametrize("option", ["-h", "--help", "--full_help"])
 def test__help(option):
-    result = subprocess.run(["python", "isoquant.py", option], capture_output=True)
+    result = subprocess.run(["./isoquant.py", option], capture_output=True)
+    print(result.returncode)
     assert result.returncode == 0
     assert b"usage" in result.stdout
     assert b"optional arguments:" in result.stdout
