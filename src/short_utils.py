7bc9a1a4ba0e1e9726dd3aaf7961f26fecdfc531
Rebecca Pfeil <rebecca_pfeil@hotmail.de>
2023-06-21 10:04:31 +0300

    Cosmetic changes

--- src/short_utils.py
+++ src/short_utils.py
@@ -13,17 +13,17 @@ def get_introns(f, chromosome, start, end):
 				i_list.add((i[0]+1,i[1]))
 		return i_list
 
-def ref_from_region(reference, region): 
+def get_region_from_db(db, region): 
 	
-	gene_list = list(reference.features_of_type('gene', limit = region, order_by=('seqid', 'start')))
+	gene_list = list(db.features_of_type('gene', limit = region, order_by=('seqid', 'start')))
 	
 	all_isoforms_introns = {}
 	all_isoforms_exons = {}
 	
 	for gene_db in gene_list:
-		for t in reference.children(gene_db, featuretype=('transcript', 'mRNA'), order_by='start'):
+		for t in db.children(gene_db, featuretype=('transcript', 'mRNA'), order_by='start'):
 			all_isoforms_exons[t.id] = []
-			for e in reference.children(t, order_by='start'):
+			for e in db.children(t, order_by='start'):
 				if e.featuretype == 'exon':
 					all_isoforms_exons[t.id].append((e.start, e.end))
 
