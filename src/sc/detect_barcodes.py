0f0d05368cffde3a052271d661fdb590da3a7fe9
Andrey Prjibelski <andrewprzh@gmail.com>
2023-07-02 18:45:22 +0300

    minor refactoring of barcode calling

--- src/sc/detect_barcodes.py
+++ src/sc/detect_barcodes.py
@@ -16,17 +16,27 @@ import itertools
 import shutil
 from concurrent.futures import ProcessPoolExecutor
 from collections import defaultdict
+from enum import Enum, unique
 
 import pysam
 from Bio import SeqIO
 import logging
 
-from src.sc.kmer_index import KmerIndexer
-from src.sc.sequence_common import reverse_complement, detect_exact_positions, find_candidate_with_max_score_ssw
+from src.sc.barcode_detection_methods import DoubleBarcodeDetector, TenXBarcodeDetector
 from src.sc.reports import *
 
 
 logger = logging.getLogger('IsoQuant')
+
+
+@unique
+class BarcodeCallingAlgorithm(Enum):
+    tenX = 2
+    curio = 3
+
+
+ALGORITHMS = [BarcodeCallingAlgorithm.tenX.name, BarcodeCallingAlgorithm.curio.name]
+
 
 
 READ_CHUNK_SIZE = 10000
@@ -119,27 +129,38 @@ def bam_file_chunk_reader(handler):
     yield current_chunk
 
 
-def process_chunk(barcodes, read_chunk, output_file, num, min_score):
+def process_chunk(barcodes, read_chunk, output_file, num, min_score, method):
     output_file += "_" + str(num)
-    barcode_detector = DoubleBarcodeDetector(barcodes, min_score=min_score)
+    barcode_detector = None
+    if method == BarcodeCallingAlgorithm.tenX:
+        barcode_detector = TenXBarcodeDetector()
+    elif method == BarcodeCallingAlgorithm.curio:
+        barcode_detector = DoubleBarcodeDetector(barcodes, min_score=min_score)
+    else:
+        logger.critical("Unknown barcode calling method")
     barcode_caller = BarcodeCaller(output_file, barcode_detector)
     barcode_caller.process_chunk(read_chunk)
     return output_file
 
 
-def process_single_thread(args):
-    barcodes = load_barcodes(args.barcodes)
-    umis = load_barcodes(args.umi) if args.umi else None
-    barcode_detector = DoubleBarcodeDetector(barcodes, umi_list=umis, min_score=args.min_score)
-    barcode_caller = BarcodeCaller(args.output, barcode_detector)
-    barcode_caller.process(args.input)
+def process_single_thread(input_reads, barcodes, min_score, method, output):
+    barcodes = load_barcodes(barcodes)
+    barcode_detector = None
+    if method == BarcodeCallingAlgorithm.tenX:
+        barcode_detector = TenXBarcodeDetector()
+    elif method == BarcodeCallingAlgorithm.curio:
+        barcode_detector = DoubleBarcodeDetector(barcodes, min_score=min_score)
+    else:
+        logger.critical("Unknown barcode calling method")
+    barcode_caller = BarcodeCaller(output, barcode_detector)
+    barcode_caller.process(input_reads)
 
 
-def process_in_parallel(args):
-    barcodes = load_barcodes(args.barcodes)
+def process_in_parallel(input_reads, barcodes, min_score, method, threads, aux, output):
+    barcodes = load_barcodes(barcodes)
     logger.info("Loaded %d barcodes" % len(barcodes))
 
-    input_file = args.input
+    input_file = input_reads
     logger.info("Processing " + input_file)
     fname, outer_ext = os.path.splitext(os.path.basename(input_file))
     low_ext = outer_ext.lower()
@@ -161,9 +182,9 @@ def process_in_parallel(args):
         logger.error("Unknown file format " + input_file)
         exit(-1)
 
-    tmp_dir = "barcode_calling_%x" % random.randint(0, 1 << 32)
+    tmp_dir = os.path.join(aux, "barcode_calling_%x" % random.randint(0, 1 << 32))
     while os.path.exists(tmp_dir):
-        tmp_dir = "barcode_calling_%x" % random.randint(0, 1 << 32)
+        tmp_dir = os.path.join(aux, "barcode_calling_%x" % random.randint(0, 1 << 32))
     os.makedirs(tmp_dir)
 
     barcode_calling_gen = (
@@ -172,12 +193,13 @@ def process_in_parallel(args):
         read_chunk_gen,
         itertools.repeat(os.path.join(tmp_dir, "bc")),
         itertools.count(start=0, step=1),
-        itertools.repeat(args.min_score)
+        itertools.repeat(min_score),
+        itertools.repeat(method)
     )
 
-    with ProcessPoolExecutor(max_workers=args.threads) as proc:
+    with ProcessPoolExecutor(max_workers=threads) as proc:
         output_files = proc.map(*barcode_calling_gen, chunksize=1)
-    outf = open(args.output, "w")
+    outf = open(output, "w")
     stat_dict = defaultdict(int)
     for tmp_file in output_files:
         shutil.copyfileobj(open(tmp_file, "r"), outf)
@@ -210,26 +232,30 @@ def set_logger(logger_instance):
 
 
 def parse_args():
+
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("--output", "-o", type=str, help="output prefix name", required=True)
     parser.add_argument("--barcodes", "-b", type=str, help="barcode whitelist", required=True)
-    parser.add_argument("--umi", "-u", type=str, help="potential UMIs")
+    # parser.add_argument("--umi", "-u", type=str, help="potential UMIs")
     parser.add_argument("--input", "-i", type=str, help="input reads in [gzipped] FASTA, FASTQ, BAM, SAM)", required=True)
     parser.add_argument("--threads", "-t", type=int, help="threads to use", default=8)
     parser.add_argument("--min_score", type=int, help="minimal barcode score", default=14)
-
+    parser.add_argument("--method", "-m", type=str, choices=ALGORITHMS,
+                        help="IsoQuant modes: " + ", ".join(ALGORITHMS) +
+                             "; default:curio", default="curio")
     args = parser.parse_args()
     return args
 
 
 def main():
-    #set_logger(logger)
     args = parse_args()
     set_logger(logger)
+    algorithm = BarcodeCallingAlgorithm[args.method]
     if args.threads == 1:
-        process_single_thread(args)
+        process_single_thread(args.input, args.barcodes, args.min_score, algorithm, args.output)
     else:
-        process_in_parallel(args)
+        aux = os.path.dirname(args.output)
+        process_in_parallel(args.input, args.barcodes, args.min_score, algorithm, aux, args.output)
 
 
 if __name__ == "__main__":
