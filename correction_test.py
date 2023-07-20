871ab66a80f44f5e87ae1b8efed335d0af9cd174
Rebecca Pfeil <rebecca_pfeil@hotmail.de>
2023-07-20 16:24:10 +0300

    Added statistics for reads and introns. Included skipped exons. Current version: 10 without offset 4

--- correction_test.py
+++ correction_test.py
@@ -6,30 +6,31 @@ from src.illumina_exon_corrector import IlluminaExonCorrector
 from src.alignment_info import AlignmentInfo
 from src.common import junctions_from_blocks, overlaps
 from src.short_utils import get_region_from_db
+from src.correction_stats import CorrectionStats, Stats
 
 def parse_args():
-	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
-	parser.add_argument("--short", "-s", help="input sam/bam file containing alignment of the short reads", 
-						type=str, dest = "short", required = True)
-	parser.add_argument("--ont", "-i", help="input sam/bam file containing alignment of the long reads", 
-						type=str, dest = "ont", required = True)
-	parser.add_argument("--reference", "-r", help="input reference gtf file",
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("--short", "-s", help="input sam/bam file containing alignment of the short reads", 
+                        type=str, dest = "short", required = True)
+    parser.add_argument("--ont", "-i", help="input sam/bam file containing alignment of the long reads", 
+                        type=str, dest = "ont", required = True)
+    parser.add_argument("--reference", "-r", help="input reference gtf file",
                         type=str, dest = "ref", required = True)
-	parser.add_argument("--output", "-o", help="output folder location",
-						type=str, dest = "out", required = True)
-	return parser.parse_args()
-	
+    parser.add_argument("--output", "-o", help="output folder location",
+                        type=str, dest = "out", required = True)
+    return parser.parse_args()
+
 def get_reference_db(ref_file, out_folder):
-	if(ref_file[-2:] != "db"):
-		gtf = ref_file
-		args.output_exists = os.path.exists(out_folder)
-		if not args.output_exists:
-			os.makedirs(out_folder)
-		ref = os.path.join(out_folder, "mouse_reference.db")
-		gtf2db(gtf, ref, True)
-	else:
-		ref = ref_file
-	return gffutils.FeatureDB(ref)
+    if(ref_file[-2:] != "db"):
+        gtf = ref_file
+        args.output_exists = os.path.exists(out_folder)
+        if not args.output_exists:
+            os.makedirs(out_folder)
+        ref = os.path.join(out_folder, "mouse_reference.db")
+        gtf2db(gtf, ref, True)
+    else:
+        ref = ref_file
+    return gffutils.FeatureDB(ref)
 
 
 args = parse_args()
@@ -37,45 +38,56 @@ short_file = args.short
 long_file = pysam.AlignmentFile(args.ont, "rb")
 ref_db = get_reference_db(args.ref, args.out)
 gene_list = list(ref_db.features_of_type('gene', order_by=('seqid', 'start')))
+correction = CorrectionStats(ref_db, long_file)
+categorized = []
 current_region = (0,0)
 chromosome = 0
 genes = []
 before = 0
 after = 0
 for gene in gene_list:
-	if overlaps((gene.start,gene.end), current_region) and gene.seqid == chromosome:
-		genes.append(gene)
-		current_region = (current_region[0], max(current_region[1], gene.end))
-	else:
-		if genes :
-			corrector = IlluminaExonCorrector(chromosome, current_region[0], current_region[1], short_file)
-			reference_introns = get_region_from_db(ref_db, (chromosome, current_region[0], current_region[1]))
-			for alignment in long_file.fetch(chromosome, start = current_region[0], stop = current_region[1]):
-				ai = AlignmentInfo(alignment)
-				if not ai.read_exons:
-					logger.warning("Read has no aligned exons")
-					continue
-				exons = ai.read_exons
-				introns = set(junctions_from_blocks(exons))
-				corrected_introns = corrector.correct_exons(introns)
-				before += len(introns.intersection(reference_introns))
-				after += len(corrected_introns.intersection(reference_introns))
-		genes = [gene]
-		current_region = (gene.start, gene.end)
-		chromosome = gene.seqid
+    if overlaps((gene.start,gene.end), current_region) and gene.seqid == chromosome:
+        genes.append(gene)
+        current_region = (current_region[0], max(current_region[1], gene.end))
+    else:
+        if genes :
+            corrector = IlluminaExonCorrector(chromosome, current_region[0], current_region[1], short_file)
+            reference_introns = get_region_from_db(ref_db, (chromosome, current_region[0], current_region[1]))
+            for alignment in long_file.fetch(chromosome, start = current_region[0], stop = current_region[1]):
+                ai = AlignmentInfo(alignment)
+                if not ai.read_exons:
+                    logger.warning("Read has no aligned exons")
+                    continue
+                exons = ai.read_exons
+                introns = set(junctions_from_blocks(exons))
+                corrected_exons = corrector.correct_read(ai)
+                corrected_introns = set(junctions_from_blocks(corrected_exons))
+                before += len(introns.intersection(reference_introns))
+                after += len(corrected_introns.intersection(reference_introns))
+                categorized.extend(correction.intron_stats(introns, corrected_introns, alignment))
+        genes = [gene]
+        current_region = (gene.start, gene.end)
+        chromosome = gene.seqid
 if genes:
-	corrector = IlluminaExonCorrector(chromosome, current_region[0], current_region[1], short_file)
-	reference_introns = get_region_from_db(ref_db, (chromosome, current_region[0], current_region[1]))
-	for alignment in long_file.fetch(chromosome, start = current_region[0], stop = current_region[1]):
-		ai = AlignmentInfo(alignment)
-		if not ai.read_exons:
-			logger.warning("Read has no aligned exons")
-			continue
-		exons = ai.read_exons
-		introns = set(junctions_from_blocks(exons))
-		corrected_introns = corrector.correct_exons(introns)
-		before += len(introns.intersection(reference_introns))
-		after += len(corrected_introns.intersection(reference_introns))
-		
+    corrector = IlluminaExonCorrector(chromosome, current_region[0], current_region[1], short_file)
+    reference_introns = get_region_from_db(ref_db, (chromosome, current_region[0], current_region[1]))
+    for alignment in long_file.fetch(chromosome, start = current_region[0], stop = current_region[1]):
+        ai = AlignmentInfo(alignment)
+        if not ai.read_exons:
+            logger.warning("Read has no aligned exons")
+            continue
+        exons = ai.read_exons
+        introns = set(junctions_from_blocks(exons))
+        corrected_exons = corrector.correct_read(ai)
+        corrected_introns = set(junctions_from_blocks(corrected_exons))
+        before += len(introns.intersection(reference_introns))
+        after += len(corrected_introns.intersection(reference_introns))
+        categorized.extend(correction.intron_stats(introns, corrected_introns, alignment))
+        
 print("Number of correct introns before correction:", before)
 print("Number of correct introns after correction:", after)
+print("Number of true positive reads:", categorized.count(Stats.true_positive))
+print("Number of false positive reads:", categorized.count(Stats.false_positive))
+print("Number of true negative reads:", categorized.count(Stats.true_negative))
+print("Number of false negative reads:", categorized.count(Stats.false_negative))
+
--- src/illumina_exon_corrector.py
+++ src/illumina_exon_corrector.py
@@ -1,6 +1,7 @@
 import pysam
 
 from .common import overlaps, junctions_from_blocks, get_exons
+from .transcript_printer import  validate_exons
 
 
 class VoidExonCorrector:
@@ -43,23 +44,55 @@ class IlluminaExonCorrector:
         corrected_introns = []
         score = 1000000000000
         sh = (0,0)
+        overlapping = []
+        appended = False
         for i in introns:
             for s in self.short_introns:
                 x = abs(i[0] - s[0]) + abs(i[1] - s[1])
                 if overlaps(i, s):
+                    overlapping.append(s)
                     if x < score:
                         score = x
                         sh = s
-            #if ((i[0] == sh[0] and i[1] == sh[1]-4) or (i[1] == sh[1] and sh[0] == i[0]-4)) and sh[0] >= exons[0][0] and sh[1] <= exons[-1][1]:
+            #if ((i[0] == sh[0] and i[1] == sh[1]-4) or (i[1] == sh[1] and sh[0] == i[0]-4)):
             #    corrected_introns.append(sh)
-            if ((i[1] == sh[1]-4) or (sh[0] == i[0]-4)) and sh[0] >= exons[0][0] and sh[1] <= exons[-1][1]:
-                corrected_introns.append(sh)
-            #elif self.counts[(sh[0]-1,sh[1])] > 100 and sh[0] >= exons[0][0] and sh[1] <= exons[-1][1]:
+            #if ((i[1] == sh[1]-4) or (sh[0] == i[0]-4)) and sh[0] >= exons[0][0] and sh[1] <= exons[-1][1]:
             #    corrected_introns.append(sh)
-            #elif ((i[0] == sh[0] or i[1] == sh[1]) and self.counts[(sh[0]-1,sh[1])] > 100) and sh[0] >= exons[0][0] and sh[1] <= exons[-1][1]:
-            #    corrected_introns.append(sh)
-            else:
-                corrected_introns.append(i)
+            #    appended = True
+            #else:
+            #    corrected_introns.append(i)
+            if len(overlapping) > 1 and not appended:
+                score = 100
+                left = (0,0)
+                right = (0,0)
+                for k in range(0, len(overlapping) -1):
+                    x = overlapping[k]
+                    for l in range(k, len(overlapping)):
+                        y = overlapping[l]
+                        if x[1] < y[0]:
+                            if abs(y[0] - x[1]) <= 50 and abs(i[0] - x[0]) <= 25 and abs(y[1] - i[1]) <= 25:
+                                if (i[0] - x[0]) + (y[1] - i[1]) - (y[0] - x[1]) < score:
+                                    left = x
+                                    right = y
+                                    score = (i[0] - x[0]) + (y[1] - i[1]) - (y[0] - x[1])
+                        elif x[0] > y[1]:
+                            if abs(x[0] - y[1]) <= 50 and abs(i[0] - y[0]) <= 25 and abs(x[1] - i[1]) <= 25:
+                                if (i[0] - y[0]) + (x[1] - i[1]) - (x[0] - y[1]) < score:
+                                    left = y
+                                    right = x
+                                    score = (i[0] - y[0]) + (x[1] - i[1]) - (x[0] - y[1])
+                if not left == (0,0):
+                    corrected_introns.append(left)
+                    corrected_introns.append(right)
+                    appended = True
+                    #print("Before:", i, "After:", left, right)
+            if not appended:
+               corrected_introns.append(i)
             sh = (0,0)
             score = 1000000000000
+            overlapping = []
+            appended = False
+        if not validate_exons(get_exons((exons[0][0], exons[-1][1]), corrected_introns)):
+            print("old:", introns)
+            print("new:", corrected_introns)
         return get_exons((exons[0][0], exons[-1][1]), corrected_introns)
