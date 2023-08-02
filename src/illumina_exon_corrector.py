cd736add046375a0ef8a2c210cf6436d5546638a
Rebecca Pfeil <rebecca_pfeil@hotmail.de>
2023-08-02 13:17:59 +0300

    version 2 for stats

--- src/illumina_exon_corrector.py
+++ src/illumina_exon_corrector.py
@@ -56,38 +56,38 @@ class IlluminaExonCorrector:
                         sh = s
             #if (i[0] == sh[0] or i[1] == sh[1]) and sh[0] >= exons[0][0] and sh[1] <= exons[-1][1] and self.counts[(sh[0]-1,sh[1])] > 100:
                 
-            if ((i[0] == sh[0] and i[1] == sh[1]-4) or (i[1] == sh[1] and sh[0] == i[0]-4)):
+            if ((i[0] == sh[0] and i[1] == sh[1]-4) or (i[1] == sh[1] and sh[0] == i[0]-4)) and self.counts[(sh[0]-1,sh[1])] > 100:
             #    corrected_introns.append(sh)
             #if ((i[1] == sh[1]-4) or (sh[0] == i[0]-4)) and sh[0] >= exons[0][0] and sh[1] <= exons[-1][1]:
                 corrected_introns.append(sh)
                 appended = True
             #else:
             #    corrected_introns.append(i)
-            if len(overlapping) > 1 and not appended:
-                score = 100
-                left = (0,0)
-                right = (0,0)
-                for k in range(0, len(overlapping) -1):
-                    x = overlapping[k]
-                    for l in range(k, len(overlapping)):
-                        y = overlapping[l]
-                        if x[1] < y[0] and (not x[0] == i[0] or not y[1] == i[1]):
-                            if abs(y[0] - x[1]) <= 50 and abs(i[0] - x[0]) <= 25 and abs(y[1] - i[1]) <= 25:
-                                if (i[0] - x[0]) + (y[1] - i[1]) - (y[0] - x[1]) < score:
-                                    left = x
-                                    right = y
-                                    score = (i[0] - x[0]) + (y[1] - i[1]) - (y[0] - x[1])
-                        elif x[0] > y[1] and (not y[0] == i[0] or not x[1] == i[1]):
-                            if abs(x[0] - y[1]) <= 50 and abs(i[0] - y[0]) <= 25 and abs(x[1] - i[1]) <= 25:
-                                if (i[0] - y[0]) + (x[1] - i[1]) - (x[0] - y[1]) < score:
-                                    left = y
-                                    right = x
-                                    score = (i[0] - y[0]) + (x[1] - i[1]) - (x[0] - y[1])
-                if not left == (0,0):
-                    corrected_introns.append(left)
-                    corrected_introns.append(right)
-                    appended = True
-                    #print("Before:", i, "After:", left, right)
+            # if len(overlapping) > 1 and not appended:
+                # score = 100
+                # left = (0,0)
+                # right = (0,0)
+                # for k in range(0, len(overlapping) -1):
+                    # x = overlapping[k]
+                    # for l in range(k, len(overlapping)):
+                        # y = overlapping[l]
+                        # if x[1] < y[0] and (not x[0] == i[0] or not y[1] == i[1]):
+                            # if abs(y[0] - x[1]) <= 50 and abs(i[0] - x[0]) <= 25 and abs(y[1] - i[1]) <= 25:
+                                # if (i[0] - x[0]) + (y[1] - i[1]) - (y[0] - x[1]) < score:
+                                    # left = x
+                                    # right = y
+                                    # score = (i[0] - x[0]) + (y[1] - i[1]) - (y[0] - x[1])
+                        # elif x[0] > y[1] and (not y[0] == i[0] or not x[1] == i[1]):
+                            # if abs(x[0] - y[1]) <= 50 and abs(i[0] - y[0]) <= 25 and abs(x[1] - i[1]) <= 25:
+                                # if (i[0] - y[0]) + (x[1] - i[1]) - (x[0] - y[1]) < score:
+                                    # left = y
+                                    # right = x
+                                    # score = (i[0] - y[0]) + (x[1] - i[1]) - (x[0] - y[1])
+                # if not left == (0,0):
+                    # corrected_introns.append(left)
+                    # corrected_introns.append(right)
+                    # appended = True
+                    # #print("Before:", i, "After:", left, right)
             if not appended:
                corrected_introns.append(i)
             sh = (0,0)
