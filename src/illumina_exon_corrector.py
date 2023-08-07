2e90196e44e674d0f7110067219f7e27ce71a33f
Rebecca Pfeil <rebecca_pfeil@hotmail.de>
2023-08-07 16:13:47 +0300

    Added first test. Reworking the code slightly

--- src/illumina_exon_corrector.py
+++ src/illumina_exon_corrector.py
@@ -1,8 +1,10 @@
 import pysam
+import logging
 
 from .common import overlaps, junctions_from_blocks, get_exons
 from .transcript_printer import  validate_exons
 
+logger = logging.getLogger('IsoQuant')
 
 class VoidExonCorrector:
 
@@ -16,12 +18,27 @@ class VoidExonCorrector:
 
 class IlluminaExonCorrector:
     
+    MAX_SCORE = 1000000000000
+    ABSENT_INTRON = (0,0)
+    EXON_LENGTH = 50
+    SIDE_DIFF = 25
+    
     def __init__(self, chromosome, start, end, short_read_file):
         self.chromosome = chromosome
         self.start = start
         self.end = end
         
         self.short_introns, self.counts = self.get_introns(short_read_file, chromosome, start, end)
+        
+    @classmethod
+    def from_data(cls, short_introns):
+        corrector = cls.__new__(cls)
+        
+        corrector.chromosome = 0
+        corrector.start = 0
+        corrector.end = 0
+        corrector.short_introns = short_introns
+        corrector.counts = dict()
         
         
     def get_introns(self, f, chromosome, start, end):
@@ -34,16 +51,35 @@ class IlluminaExonCorrector:
                 i_list.add((i[0]+1,i[1]))
         return i_list, intr
         
+    #def set_introns(alignment_file = None)
+    #or as in Gene_Info.from_models, i.e. from_data
+        
 
     def correct_read(self, alignment_info):
         return self.correct_exons(alignment_info.read_exons)
+        
+    def skipped_score(self, left, right, old):
+        return (old[0] - left[0]) + (right[1] - old[1]) - (right[0] - left[1])
+        
+    def better_skipped(self, left, right, old, score):
+        return self.skipped_score(left, right, old) < score
+        
+    def right_length(self, left, right, old):
+        middle = abs(right[0] - left[1]) <= IlluminaExonCorrector.EXON_LENGTH
+        left_side = abs(old[0] - left[0]) <= IlluminaExonCorrector.SIDE_DIFF
+        right_side = abs(right[1] - old[1]) <= IlluminaExonCorrector.SIDE_DIFF
+        return (middle and left_side and right_side)
+        
+    def one_differs(self, left, right, old):
+        return (not left[0] == old[0] or not right[1] == old[1])
 
 
     def correct_exons(self, exons):
+        print(exons)
         introns = junctions_from_blocks(exons)
         corrected_introns = []
-        score = 1000000000000
-        sh = (0,0)
+        score = IlluminaExonCorrector.MAX_SCORE
+        sh = IlluminaExonCorrector.ABSENT_INTRON
         overlapping = []
         appended = False
         for i in introns:
@@ -55,42 +91,44 @@ class IlluminaExonCorrector:
                         score = x
                         sh = s
             #if (i[0] == sh[0] or i[1] == sh[1]) and sh[0] >= exons[0][0] and sh[1] <= exons[-1][1] and self.counts[(sh[0]-1,sh[1])] > 100:
-                
             if ((i[0] == sh[0] and i[1] == sh[1]-4) or (i[1] == sh[1] and sh[0] == i[0]-4)):
             #if ((i[1] == sh[1]-4) or (sh[0] == i[0]-4)) and sh[0] >= exons[0][0] and sh[1] <= exons[-1][1]:
                 corrected_introns.append(sh)
                 appended = True
             if len(overlapping) > 1 and not appended:
-                score = 100
-                left = (0,0)
-                right = (0,0)
+                score = IlluminaExonCorrector.MAX_SCORE
+                left = IlluminaExonCorrector.ABSENT_INTRON
+                right = IlluminaExonCorrector.ABSENT_INTRON
                 for k in range(0, len(overlapping) -1):
                     x = overlapping[k]
                     for l in range(k, len(overlapping)):
                         y = overlapping[l]
-                        if x[1] < y[0] and (not x[0] == i[0] or not y[1] == i[1]):
-                            if abs(y[0] - x[1]) <= 50 and abs(i[0] - x[0]) <= 25 and abs(y[1] - i[1]) <= 25:
-                                if (i[0] - x[0]) + (y[1] - i[1]) - (y[0] - x[1]) < score:
+                        if x[1] < y[0]: 
+                            if self.right_length(x, y, i) and self.one_differs(x, y, i):
+                                if self.better_skipped(x, y, i, score):
                                     left = x
                                     right = y
-                                    score = (i[0] - x[0]) + (y[1] - i[1]) - (y[0] - x[1])
-                        elif x[0] > y[1] and (not y[0] == i[0] or not x[1] == i[1]):
-                            if abs(x[0] - y[1]) <= 50 and abs(i[0] - y[0]) <= 25 and abs(x[1] - i[1]) <= 25:
-                                if (i[0] - y[0]) + (x[1] - i[1]) - (x[0] - y[1]) < score:
+                                    score = self.skipped_score(x, y, i)
+                        elif x[0] > y[1]:
+                            if self.right_length(y, x, i) and self.one_differs(y, x, i):
+                                if self.better_skipped(y, x, i, score):
                                     left = y
                                     right = x
-                                    score = (i[0] - y[0]) + (x[1] - i[1]) - (x[0] - y[1])
-                if not left == (0,0):
+                                    score = self.skipped_score(y, x, i)
+                if not left == IlluminaExonCorrector.ABSENT_INTRON:
                     corrected_introns.append(left)
                     corrected_introns.append(right)
                     appended = True
             if not appended:
                corrected_introns.append(i)
-            sh = (0,0)
-            score = 1000000000000
+            sh = IlluminaExonCorrector.ABSENT_INTRON
+            score = IlluminaExonCorrector.MAX_SCORE
             overlapping = []
             appended = False
         if not validate_exons(get_exons((exons[0][0], exons[-1][1]), corrected_introns)):
-            print("old:", introns)
-            print("new:", corrected_introns)
+            logger.debug("old:", introns)
+            logger.debug("new:", corrected_introns)
         return get_exons((exons[0][0], exons[-1][1]), corrected_introns)
+        
+# function for parameters like in set_matching_options, put things like 25, 50 ...
+# comment functions
