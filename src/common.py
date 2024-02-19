29fe734ebc27411900b2f632f54f4c2c3bfc131f
Andrey Prjibelski <andrewprzh@gmail.com>
2024-02-19 17:18:21 +0200

    do not perfrom double check on overlapping regions

--- src/common.py
+++ src/common.py
@@ -150,6 +150,14 @@ def overlaps_at_least(range1, range2, delta=0):
         return ovlp2 >= d or range1[0] <= range2[0]
 
 
+# dangerous function, works only when range1 and range2 are already known to overlap, do not use if unsure
+def overlaps_at_least_when_overlap(range1, range2, delta=0):
+    if range1[1] < range2[1]:
+        return range1[1] - range2[0] - 1 >= delta or range1[0] >= range2[0]
+    else:
+        return range2[1] - range1[0] - 1 >= delta or range1[0] <= range2[0]
+
+
 def intersection_len(range1, range2):
     return max(0, min(range1[1], range2[1]) - max(range1[0], range2[0]) + 1)
 
