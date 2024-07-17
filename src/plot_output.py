eff85aad0101052c80cf1112bd0d80a64dfacd37
Jackfreeman88 <jackwfreeman@yahoo.com>
2024-07-17 17:16:55 -0500

    save gene dict, added output dir, and made viz executable

--- src/plot_output.py
+++ src/plot_output.py
@@ -3,8 +3,18 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
+
 class PlotOutput:
-    def __init__(self, updated_gene_dict, gene_names, output_directory, reads_and_class=None, filter_transcripts=None, conditions=False, use_counts=False):
+    def __init__(
+        self,
+        updated_gene_dict,
+        gene_names,
+        output_directory,
+        reads_and_class=None,
+        filter_transcripts=None,
+        conditions=False,
+        use_counts=False,
+    ):
         self.updated_gene_dict = updated_gene_dict
         self.gene_names = gene_names
         self.output_directory = output_directory
@@ -21,50 +31,93 @@ class PlotOutput:
         # Get the first condition's gene dictionary
         first_condition = next(iter(self.updated_gene_dict))
         gene_dict = self.updated_gene_dict[first_condition]
-        
+
         for gene_name in self.gene_names:
             if gene_name in gene_dict:
                 gene_data = gene_dict[gene_name]
-                num_transcripts = len(gene_data['transcripts'])
-                plot_height = max(3, num_transcripts * 0.3)  # Adjust the height dynamically
+                num_transcripts = len(gene_data["transcripts"])
+                plot_height = max(
+                    3, num_transcripts * 0.3
+                )  # Adjust the height dynamically
 
-                fig, ax = plt.subplots(figsize=(12, plot_height))  # Adjust height dynamically
+                fig, ax = plt.subplots(
+                    figsize=(12, plot_height)
+                )  # Adjust height dynamically
 
                 if self.filter_transcripts is not None:
-                    ax.set_title(f"Transcripts of Gene: {gene_data['name']} on Chromosome {gene_data['chromosome']} with value over {self.filter_transcripts}")
+                    ax.set_title(
+                        f"Transcripts of Gene: {gene_data['name']} on Chromosome {gene_data['chromosome']} with value over {self.filter_transcripts}"
+                    )
                 else:
-                    ax.set_title(f"Transcripts of Gene: {gene_data['name']} on Chromosome {gene_data['chromosome']}")
-                    
+                    ax.set_title(
+                        f"Transcripts of Gene: {gene_data['name']} on Chromosome {gene_data['chromosome']}"
+                    )
+
                 ax.set_xlabel("Chromosomal position")
                 ax.set_ylabel("Transcripts")
                 ax.set_yticks(range(num_transcripts))
-                ax.set_yticklabels([f"{transcript_id}" for transcript_id in gene_data['transcripts'].keys()])
+                ax.set_yticklabels(
+                    [
+                        f"{transcript_id}"
+                        for transcript_id in gene_data["transcripts"].keys()
+                    ]
+                )
 
-                ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))  # Ensure genomic positions are integers
-                ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'{int(x)}'))  # Format x-axis ticks as integers
+                ax.xaxis.set_major_locator(
+                    ticker.MaxNLocator(integer=True)
+                )  # Ensure genomic positions are integers
+                ax.xaxis.set_major_formatter(
+                    ticker.FuncFormatter(lambda x, pos: f"{int(x)}")
+                )  # Format x-axis ticks as integers
 
                 # Plot each transcript
-                for i, (transcript_id, transcript_info) in enumerate(gene_data['transcripts'].items()):
+                for i, (transcript_id, transcript_info) in enumerate(
+                    gene_data["transcripts"].items()
+                ):
                     # Determine the direction based on the gene's strand information
-                    direction_marker = '>' if gene_data['strand'] == '+' else '<'
+                    direction_marker = ">" if gene_data["strand"] == "+" else "<"
 
                     # Add a direction marker to indicate the direction of the transcript
-                    marker_pos = transcript_info['end'] + 100 if gene_data['strand'] == '+' else transcript_info['start'] - 100
-                    ax.plot(marker_pos, i, marker=direction_marker, markersize=5, color="blue")
+                    marker_pos = (
+                        transcript_info["end"] + 100
+                        if gene_data["strand"] == "+"
+                        else transcript_info["start"] - 100
+                    )
+                    ax.plot(
+                        marker_pos,
+                        i,
+                        marker=direction_marker,
+                        markersize=5,
+                        color="blue",
+                    )
 
                     # Draw the line for the whole transcript
-                    ax.plot([transcript_info['start'], transcript_info['end']], [i, i], color="grey", linewidth=2)
+                    ax.plot(
+                        [transcript_info["start"], transcript_info["end"]],
+                        [i, i],
+                        color="grey",
+                        linewidth=2,
+                    )
 
                     # Exon blocks
-                    for exon in transcript_info['exons']:
-                        exon_length = exon['end'] - exon['start']
-                        ax.add_patch(plt.Rectangle((exon['start'], i - 0.4), exon_length, 0.8, color="skyblue"))
+                    for exon in transcript_info["exons"]:
+                        exon_length = exon["end"] - exon["start"]
+                        ax.add_patch(
+                            plt.Rectangle(
+                                (exon["start"], i - 0.4),
+                                exon_length,
+                                0.8,
+                                color="skyblue",
+                            )
+                        )
 
-                ax.set_xlim(gene_data['start'], gene_data['end'])
+                ax.set_xlim(gene_data["start"], gene_data["end"])
                 ax.invert_yaxis()  # First transcript at the top
 
                 plt.tight_layout()
-                plot_path = os.path.join(self.visualization_dir, f'{gene_name}_splicing.png')
+                plot_path = os.path.join(
+                    self.visualization_dir, f"{gene_name}_splicing.png"
+                )
                 plt.savefig(plot_path)  # Saving plot by gene name
                 plt.close(fig)
 
@@ -72,54 +125,71 @@ class PlotOutput:
         """
         Visualize transcript usage for each gene in gene_names across different conditions.
         """
-        
+
         for gene_name in self.gene_names:
             gene_data = {}
             for condition, genes in self.updated_gene_dict.items():
                 if gene_name in genes:
-                    gene_data[condition] = genes[gene_name]['transcripts']
+                    gene_data[condition] = genes[gene_name]["transcripts"]
 
             if not gene_data:
                 print(f"Gene {gene_name} not found in the data.")
                 continue
-            
+
             conditions = list(gene_data.keys())
             n_bars = len(conditions)
-            
+
             fig, ax = plt.subplots(figsize=(12, 8))
             index = np.arange(n_bars)
             bar_width = 0.35
             opacity = 0.8
-            
+
             for sample_type, transcripts in gene_data.items():
                 print(f"Sample Type: {sample_type}")
                 for transcript_id, transcript_info in transcripts.items():
-                    print(f"  Transcript ID: {transcript_id}, Value: {transcript_info['value']}")
+                    print(
+                        f"  Transcript ID: {transcript_id}, Value: {transcript_info['value']}"
+                    )
             # Adjusting the colors for better within-bar comparison
             max_transcripts = max(len(gene_data[condition]) for condition in conditions)
-            colors = plt.cm.plasma(np.linspace(0, 1, num=max_transcripts))  # Using plasma for better color gradation
-            
-            bottom_val = np.zeros(n_bars) 
+            colors = plt.cm.plasma(
+                np.linspace(0, 1, num=max_transcripts)
+            )  # Using plasma for better color gradation
+
+            bottom_val = np.zeros(n_bars)
             for i, condition in enumerate(conditions):
                 transcripts = gene_data[condition]
-                for j, (transcript_id, transcript_info) in enumerate(transcripts.items()):
+                for j, (transcript_id, transcript_info) in enumerate(
+                    transcripts.items()
+                ):
                     color = colors[j % len(colors)]
-                    value = transcript_info['value']
-                    plt.bar(i, float(value), bar_width, bottom=bottom_val[i], alpha=opacity, color=color, label=transcript_id if i == 0 else "")
+                    value = transcript_info["value"]
+                    plt.bar(
+                        i,
+                        float(value),
+                        bar_width,
+                        bottom=bottom_val[i],
+                        alpha=opacity,
+                        color=color,
+                        label=transcript_id if i == 0 else "",
+                    )
                     bottom_val[i] += float(value)
-            
-            plt.xlabel('Sample Type')
-            plt.ylabel('Transcript Usage (TPM)')
-            plt.title(f'Transcript Usage for {gene_name} by Sample Type')
+
+            plt.xlabel("Sample Type")
+            plt.ylabel("Transcript Usage (TPM)")
+            plt.title(f"Transcript Usage for {gene_name} by Sample Type")
             plt.xticks(index, conditions)
-            plt.legend(title="Transcript IDs", bbox_to_anchor=(1.05, 1), loc='upper left')
-            
+            plt.legend(
+                title="Transcript IDs", bbox_to_anchor=(1.05, 1), loc="upper left"
+            )
+
             plt.tight_layout()
-            plot_path = os.path.join(self.visualization_dir, f'{gene_name}_transcript_usage_by_sample_type.png')
+            plot_path = os.path.join(
+                self.visualization_dir,
+                f"{gene_name}_transcript_usage_by_sample_type.png",
+            )
             plt.savefig(plot_path)
             plt.close(fig)
-
-
 
 
 def make_pie_chart():
@@ -131,21 +201,16 @@ def make_pie_chart():
         "noninformative": 130493,
         "unique": 745194,
         "unique_minor_difference": 79178,
-        }
+    }
     labels = data.keys()
     sizes = data.values()
     total = sum(sizes)
     print(total)
-    plt.pie(sizes, labels=labels, autopct='%1.1f%%')
-    plt.axis('equal')
+    plt.pie(sizes, labels=labels, autopct="%1.1f%%")
+    plt.axis("equal")
     plt.title(f"Total: {total}")
     plt.show()
-    plt.savefig('read_assignment_pie_chart.png')
-
-
-
-
-
+    plt.savefig("read_assignment_pie_chart.png")
 
 
 def visualize_transcript_usage_single_gene(gene_data, gene_name):
@@ -154,37 +219,47 @@ def visualize_transcript_usage_single_gene(gene_data, gene_name):
     :param gene_data: Dict containing transcript usage data for a given gene across different sample types.
     :param gene_name: The gene to visualize.
     """
-    
+
     if gene_name not in gene_data:
         print(f"Gene {gene_name} not found in the data.")
         return
-    
+
     sample_types = gene_data[gene_name].keys()
     n_bars = len(sample_types)
-    
+
     fig, ax = plt.subplots(figsize=(10, 7))
     index = np.arange(n_bars)
     bar_width = 0.35
     opacity = 0.8
-    
+
     # Adjusting the colors for better within-bar comparison
     max_transcripts = max(len(gene_data[gene_name][sample]) for sample in sample_types)
-    colors = plt.cm.plasma(np.linspace(0, 1, num=max_transcripts))  # Using plasma for better color gradation
-    
-    bottom_val = np.zeros(n_bars) 
+    colors = plt.cm.plasma(
+        np.linspace(0, 1, num=max_transcripts)
+    )  # Using plasma for better color gradation
+
+    bottom_val = np.zeros(n_bars)
     for i, sample_type in enumerate(sample_types):
         transcripts = gene_data[gene_name][sample_type]
         for j, (transcript_id, value) in enumerate(transcripts):
             color = colors[j % len(colors)]
-            plt.bar(i, float(value), bar_width, bottom=bottom_val[i], alpha=opacity, color=color, label=transcript_id if i == 0 else "")
+            plt.bar(
+                i,
+                float(value),
+                bar_width,
+                bottom=bottom_val[i],
+                alpha=opacity,
+                color=color,
+                label=transcript_id if i == 0 else "",
+            )
             bottom_val[i] += float(value)
-    
-    plt.xlabel('Sample Type')
-    plt.ylabel('Transcript Usage (TPM)')
-    plt.title(f'Transcript Usage for {gene_name} by Sample Type')
+
+    plt.xlabel("Sample Type")
+    plt.ylabel("Transcript Usage (TPM)")
+    plt.title(f"Transcript Usage for {gene_name} by Sample Type")
     plt.xticks(index, sample_types)
-    plt.legend(title="Transcript IDs", bbox_to_anchor=(1.05, 1), loc='upper left')
-    
+    plt.legend(title="Transcript IDs", bbox_to_anchor=(1.05, 1), loc="upper left")
+
     plt.tight_layout()
     plt.show()
-    plt.savefig(f'{gene_name}_transcript_usage_by_sample_type_ref.png') 
+    plt.savefig(f"{gene_name}_transcript_usage_by_sample_type_ref.png")
--- visualize.py
+++ visualize.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from src.post_process import OutputConfig, DictionaryBuilder
 from src.plot_output import PlotOutput
 import argparse
@@ -5,26 +7,66 @@ import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Visualize your IsoQuant output.")
-    parser.add_argument("output_directory", type=str, help="Directory containing IsoQuant output files.")
-    parser.add_argument("--gtf", type=str, help="Optional path to a GTF file if unable to be extracted from IsoQuant log", default=None)
-    parser.add_argument("--counts", action="store_true", help="Use counts instead of TPM files.")
-    parser.add_argument("--ref_only", action="store_true", help="Use only reference transcript quantification instead of transcript model quantification.")
-    parser.add_argument("--filter_transcripts", type=float, help="Filter transcripts by minimum value occuring in at least one condition.", default=None)
-    parser.add_argument("--gene_list", type=str, required=True, help="Path to a .txt file containing a list of genes, each on its own line.")
+    parser.add_argument(
+        "output_directory", type=str, help="Directory containing IsoQuant output files."
+    )
+    parser.add_argument(
+        "--viz_output",
+        type=str,
+        help="Optional directory to save visualization output files, defaults to the main output directory.",
+        default=None,
+    )
+    parser.add_argument(
+        "--gtf",
+        type=str,
+        help="Optional path to a GTF file if unable to be extracted from IsoQuant log",
+        default=None,
+    )
+    parser.add_argument(
+        "--counts", action="store_true", help="Use counts instead of TPM files."
+    )
+    parser.add_argument(
+        "--ref_only",
+        action="store_true",
+        help="Use only reference transcript quantification instead of transcript model quantification.",
+    )
+    parser.add_argument(
+        "--filter_transcripts",
+        type=float,
+        help="Filter transcripts by minimum value occuring in at least one condition.",
+        default=None,
+    )
+    parser.add_argument(
+        "--gene_list",
+        type=str,
+        required=True,
+        help="Path to a .txt file containing a list of genes, each on its own line.",
+    )
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-    output = OutputConfig(args.output_directory, use_counts=args.counts, ref_only=args.ref_only, gtf=args.gtf)
+    output = OutputConfig(
+        args.output_directory,
+        use_counts=args.counts,
+        ref_only=args.ref_only,
+        gtf=args.gtf,
+    )
     dictionary_builder = DictionaryBuilder(output)
     gene_list = dictionary_builder.read_gene_list(args.gene_list)
     update_names = not all(gene.startswith("ENS") for gene in gene_list)
     gene_dict = dictionary_builder.build_gene_transcript_exon_dictionaries()
-    reads_and_class = dictionary_builder.build_read_assignment_and_classification_dictionaries()
+    reads_and_class = (
+        dictionary_builder.build_read_assignment_and_classification_dictionaries()
+    )
 
     if output.conditions:
-        gene_file = output.gene_grouped_tpm if not output.use_counts else output.gene_grouped_counts
+        gene_file = (
+            output.gene_grouped_tpm
+            if not output.use_counts
+            else output.gene_grouped_counts
+        )
     else:
         gene_file = output.gene_tpm if not output.use_counts else output.gene_counts
 
@@ -37,23 +79,68 @@ def main():
     if output.ref_only or not output.extended_annotation:
         print("Using reference-only based quantification.")
         if output.conditions:
-            updated_gene_dict = dictionary_builder.update_transcript_values(updated_gene_dict, output.transcript_grouped_tpm if not output.use_counts else output.transcript_grouped_counts)
+            updated_gene_dict = dictionary_builder.update_transcript_values(
+                updated_gene_dict,
+                (
+                    output.transcript_grouped_tpm
+                    if not output.use_counts
+                    else output.transcript_grouped_counts
+                ),
+            )
         else:
-            updated_gene_dict = dictionary_builder.update_transcript_values(updated_gene_dict, output.transcript_tpm if not output.use_counts else output.transcript_counts)
+            updated_gene_dict = dictionary_builder.update_transcript_values(
+                updated_gene_dict,
+                (
+                    output.transcript_tpm
+                    if not output.use_counts
+                    else output.transcript_counts
+                ),
+            )
     else:
         print("Using transcript model quantification.")
         if output.conditions:
-            updated_gene_dict = dictionary_builder.update_transcript_values(updated_gene_dict, output.transcript_model_grouped_tpm if not output.use_counts else output.transcript_model_grouped_counts)
+            updated_gene_dict = dictionary_builder.update_transcript_values(
+                updated_gene_dict,
+                (
+                    output.transcript_model_grouped_tpm
+                    if not output.use_counts
+                    else output.transcript_model_grouped_counts
+                ),
+            )
         else:
-            updated_gene_dict = dictionary_builder.update_transcript_values(updated_gene_dict, output.transcript_model_tpm if not output.use_counts else output.transcript_model_counts)
+            updated_gene_dict = dictionary_builder.update_transcript_values(
+                updated_gene_dict,
+                (
+                    output.transcript_model_tpm
+                    if not output.use_counts
+                    else output.transcript_model_counts
+                ),
+            )
 
     if args.filter_transcripts is not None:
-        print(f"Filtering transcripts with minimum value {args.filter_transcripts} in at least one condition.")
-        updated_gene_dict = dictionary_builder.filter_transcripts_by_minimum_value(updated_gene_dict, min_value=args.filter_transcripts)
+        print(
+            f"Filtering transcripts with minimum value {args.filter_transcripts} in at least one condition."
+        )
+        updated_gene_dict = dictionary_builder.filter_transcripts_by_minimum_value(
+            updated_gene_dict, min_value=args.filter_transcripts
+        )
     else:
-        updated_gene_dict = dictionary_builder.filter_transcripts_by_minimum_value(updated_gene_dict)
+        updated_gene_dict = dictionary_builder.filter_transcripts_by_minimum_value(
+            updated_gene_dict
+        )
 
-    plot_output = PlotOutput(updated_gene_dict, gene_list, args.output_directory, reads_and_class, filter_transcripts=args.filter_transcripts, conditions=output.conditions, use_counts=args.counts)
+    # Visualization output directory decision
+    viz_output_directory = args.viz_output if args.viz_output else args.output_directory
+    dictionary_builder.save_gene_dict_to_json(updated_gene_dict, args.output_directory)
+    plot_output = PlotOutput(
+        updated_gene_dict,
+        gene_list,
+        viz_output_directory,
+        reads_and_class,
+        filter_transcripts=args.filter_transcripts,
+        conditions=output.conditions,
+        use_counts=args.counts,
+    )
     plot_output.plot_transcript_map()
     plot_output.plot_transcript_usage()
 
