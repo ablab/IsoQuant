d6fcbe3f60dba85d542006926e8257803a6ad6bc
Andrey Prjibelski <andrewprzh@gmail.com>
2024-08-02 01:16:36 +0300

    add header

--- src/long_read_counter.py
+++ src/long_read_counter.py
@@ -368,6 +368,7 @@ class AssignedFeatureCounter(AbstractCounter):
             assert linear_output_file is not None
 
             output_file.write(self.format_header(all_groups))
+            linear_output_file.write("feature_id\tgroup_id\tcount")
             for feature_id in all_features:
                 row_count = 0
                 for group_id in self.feature_counter[feature_id].data.keys():
