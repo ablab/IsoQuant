9844176a4116b5c5db571158104d165599efb76e
aljpetri <alexapet@lx5-500-32742.ad.helsinki.fi>
2025-06-17 20:11:31 +0300

    Transferring path constraints into new format

--- src/ilp_model.py
+++ src/ilp_model.py
@@ -37,6 +37,24 @@ def clean_graph(G,skip_isolated_nodes):
                 nodes_to_remove.append(node)
         G.remove_nodes_from(nodes_to_remove)
 
+def Constraints_Transfer_Format(input_constraints,skip_isolated_nodes=True,
+        skip_terminal_nodes=True,):
+    transferred_constraints = []
+    for pc_entry in input_constraints:
+        for nodes_tup in pc_entry:
+            constraint_list = []
+            for node in nodes_tup:
+                if skip_terminal_nodes:
+                    if not node[0] in [VERTEX_polyt, VERTEX_read_start] and not node[0] in [VERTEX_polya, VERTEX_read_end]:
+                        constraint_list.append(node)
+                else:
+                    constraint_list.append(node)
+            if len(constraint_list) > 0:
+                this_constraint = []
+                for first, second in zip(constraint_list, constraint_list[1:]):
+                    this_constraint.append((str(first), str(second)))
+                transferred_constraints.append(this_constraint)
+
 def Intron2Nx_Node(
         intron_graph,
         skip_isolated_nodes=True,
@@ -152,7 +170,8 @@ def ILP_Solver_Nodes(intron_graph,chr_id, gene_id,transcripts_constraints=[],gro
     export = False
 
     graph, additional_starts, additional_ends, edges_to_ignore = Intron2Nx_Node(intron_graph)
-    constraints = transfer_constraints(transcripts_constraints, graph)
+    constraints=Constraints_Transfer_Format(transcripts_constraints)
+    #constraints = transfer_constraints(transcripts_constraints, graph)
     if len(constraints) == 0:
         constraints = None
     #filter_constraints(graph, additional_starts,additional_ends)
