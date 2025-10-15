84a44f12fcae99fb774b22cfd31dae995d203386
arrine <arinacoroliuc@gmail.com>
2025-10-15 18:24:04 +0300

    all tests done

--- tests/test_polya_prediction.py
+++ tests/test_polya_prediction.py
@@ -18,56 +18,46 @@ from src.polya_position_model import PolyACounter
 
 
 class testPolyACounter(unittest.TestCase):
+
+    
     source_dir = os.path.dirname(os.path.realpath(__file__))
     gffutils_db = gffutils.FeatureDB(os.path.join(source_dir, 'toy_data/synth.db'), keep_order=True)
-    gene_db = gffutils_db['ENSMUSG00000020196.10']
+    gene_db = gffutils_db["ENSMUSG00000020196.10"]
     gene_info = GeneInfo([gene_db], gffutils_db)
 
-
-    def testAdd_read_info(self):
-        pass
-    def testDump(self):
-        pass
-
-
-
-
-
-    @pytest.fixture
     def counter(self):
         return PolyACounter(output_prefix="out.tsv", read_groups=["RG1", "RG2"])
     
-    @pytest.fixture
-    def counter_ignoreGroups():
-        return PolyACounter(output_prefix="out.tsv", read_groups=["RG1"])
-    
-
-    @pytest.fixture
-    def read_assignment(self, strand, polya_pos, group = ['RG1']):
+    def read_assignment(self, group = ['RG1']):
         read = ReadAssignment("id1", ReadAssignmentType.unique, IsoformMatch(MatchClassification.full_splice_match, assigned_transcript="id2"))
-        # read.strand = strand
-        # read.polyA_found = True
-        # read.polya_info.external_polyt_pos = polya_pos
+        read.strand = "-"
+        read.chr_id = "chr10"
         read.gene_info = self.gene_info
-        read.read_group = group
-        read.chr_id = 20
+        read.polyA_found = True 
+        read.assignment_type = ReadAssignmentType.inconsistent_non_intronic
+        read.isoform_matches[0].assigned_gene = "ENSMUSG00000020196.10"
+        read.isoform_matches[0].assigned_transcript = "ENSMUST00000001712.7"
+        read.polya_info = pd.Series({'external_polyt_pos': 100})
+        read.read_group = group[0]
+        return read
 
 
-
-    def test_counts(self, counter):
+    def test_counts(self):
         x = pd.Series({
             "transcript_id": "id1",
             "peak_location": 6,
             "histogram": [0]*10 + [0, 1, 2, 3, 10, 20, 30, 5, 1, 0] + [0]*10
         })
+        counter = self.counter()
         result = counter.counts(x)
 
         assert isinstance(result, (int, np.integer))
-        assert result == 62
+        assert result == 72
 
 
-    def test_counts_byGroup(self, counter):
-
+    def test_counts_byGroup(self):
+        
+        counter = self.counter()
         counter.transcripts["id1"] = {
             "data": [1, 2, 3, 4, 5, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8],
             0: [1, 2, 3, 4, 6, 7, 7, 8, 8],
@@ -84,7 +74,9 @@ class testPolyACounter(unittest.TestCase):
         assert counts == [8, 6]
         assert heights == [2, 3]
 
-    def test_sort_peaks(self, counter):
+    def test_sort_peaks(self):
+        
+        counter = self.counter()
         row = pd.Series({
             "peak_count": 3,
             "peak_location": [10, 20, 30],
@@ -99,16 +91,18 @@ class testPolyACounter(unittest.TestCase):
 
         assert sorted_row["peak_count"] == 3
         assert sorted_row["peak_location"] == [20, 30, 10]
-        assert sorted_row["peak_prominence"] == np.array([0.9, 0.5, 0.2])
-        assert sorted_row["peak_width"] == np.array([2, 3, 1])
-        assert sorted_row["peak_heights"] == np.array([50, 30, 10])
+        assert np.array_equal(sorted_row["peak_prominence"], np.array([0.9, 0.5, 0.2]))
+        assert np.array_equal(sorted_row["peak_width"], np.array([2, 3, 1]))
+        assert np.array_equal(sorted_row["peak_heights"], np.array([50, 30, 10]))
         assert sorted_row["peak_left"] == [19, 30, 10]
         assert sorted_row["peak_right"] == [21, 31, 10]
         assert sorted_row["rank"] == [1, 2, 3]
 
 
 
-    def test_flag(self, counter):
+    def test_flag(self):
+        
+        counter = self.counter()
 
         row = pd.Series({"prediction": 100, "annotated": 105})
         assert counter.flag(row) == "Known"
@@ -116,71 +110,39 @@ class testPolyACounter(unittest.TestCase):
         assert counter.flag(row) == "Novel"
 
 
-
-
-
-    def test_add_read_info_populates_transcripts(counter, read_assignment):
-        ra = read_assignment(
-            strand="+", polya_pos=150, transcript="tx1", gene="gene1"
-        )
+    def test_add_read_info(self):
+        ra = self.read_assignment()
+        counter = self.counter()
         counter.add_read_info(ra)
 
-        assert "tx1" in counter.transcripts
-        tx_data = counter.transcripts["tx1"]
-
-        assert tx_data["chr"] == "chr1"
-        assert tx_data["gene_id"] == "gene1"
-        assert isinstance(tx_data["data"], list)
-        assert 150 in tx_data["data"]
-        # Group should also contain the polya_pos
-        assert 150 in tx_data[0]  # group_numeric_ids["grp1"] == 0
+        assert ra.isoform_matches[0].assigned_transcript in counter.transcripts
+        data = counter.transcripts[ra.isoform_matches[0].assigned_transcript]
+        assert data["chr"] == "chr10"
+        assert data["gene_id"] == "ENSMUSG00000020196.10"
+        assert 100 in data["data"]
+        annot = ra.gene_info.all_isoforms_exons[ra.isoform_matches[0].assigned_transcript][0][0] - 1
+        assert data['annotated'] == annot
 
 
-    def test_dump_creates_output_file(counter, tmp_path, read_assignment):
-        # add several reads to create a histogram
-        for pos in [120, 121, 122, 150, 151, 151, 152]:
-            ra = read_assignment("+", pos, "tx1", "gene1")
-            counter.add_read_info(ra)
+    def test_dump(self):
+        counter = self.counter()
+        transcripts = {'ENSMUST00000001712.7':  {'chr': "chr10", 'gene_id': "ENSMUSG00000020196.10", 'data': [10, 12, 12, 15, 15 ,15, 15, 15, 16, 19, 20, 21, 21, 21, 21, 21], 0: [10, 12, 15, 15 ,15, 16, 21], 1:[ 12, 15, 15, 19, 20, 21, 21, 21, 21], 'annotated': 21}}
 
+        counter.transcripts = transcripts        
         counter.dump()
-        out_file = tmp_path / "out.tsv"
-        assert out_file.exists()
+        df = counter.dfResult 
 
-        df = pd.read_csv(out_file, sep="\t")
-        # Must contain required columns
-        for col in [
-            "chromosome",
-            "transcript_id",
-            "gene_id",
-            "peak_left",
-            "peak_right",
-            "prediction",
-            "peak_heights",
-            "counts",
-            "flag",
-        ]:
-            assert col in df.columns
+        pd.set_option('display.max_columns', None)
+        print(df)
+        assert df.loc[0]['chromosome'] == 'chr10'
+        assert df.loc[0]['transcript_id'] == 'ENSMUST00000001712.7'
+        assert df.loc[0]['gene_id'] == 'ENSMUSG00000020196.10'
+        for i in df['prediction']:
+            assert (i <= np.max(transcripts['ENSMUST00000001712.7']['data']) and i >= np.min(transcripts['ENSMUST00000001712.7']['data']))
+        assert df.loc[0]['counts'] == 8
+        assert df.loc[0]['counts_byGroup'] == 2
+        assert df.loc[1]['counts_byGroup'] == 6
+        assert df.loc[0]['flag'] == 'Known'
+        assert df.loc[0]['group_id'] == 'RG1'
+        assert df.loc[1]['group_id'] == 'RG2'
 
-        # Ensure predictions are integers
-        assert np.issubdtype(df["prediction"].dtype, np.integer)
-
-
-    def test_dump_appends_on_second_call(counter, tmp_path, read_assignment):
-        # add first batch
-        for pos in [200, 201, 202]:
-            ra = read_assignment("+", pos, "tx1", "gene1")
-            counter.add_read_info(ra)
-        counter.dump()
-
-        # add second batch
-        for pos in [300, 301, 302]:
-            ra = read_assignment("+", pos, "tx2", "gene2")
-            counter.add_read_info(ra)
-        counter.dump()
-
-        out_file = tmp_path / "out.tsv"
-        df = pd.read_csv(out_file, sep="\t")
-
-        # Should contain both tx1 and tx2 results
-        assert "tx1" in df["transcript_id"].values
-        assert "tx2" in df["transcript_id"].values
