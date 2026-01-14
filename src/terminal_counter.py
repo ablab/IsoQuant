0b1153cacb1a08174a8cade3276410776ac3ad5f
arrine <arinacoroliuc@gmail.com>
2026-01-15 00:44:29 +0200

    bug solved

--- src/terminal_counter.py
+++ src/terminal_counter.py
@@ -12,6 +12,7 @@ import os
 from collections import defaultdict, OrderedDict
 from enum import Enum, unique
 import random
+import sys
 
 from .isoform_assignment import (
     ReadAssignmentType,
@@ -47,7 +48,7 @@ class TerminalCounter(AbstractCounter):
         ok = False
         delta = 10
         for i in x['target']:
-            ok = ok or ((x['peak_location'] >= i - delta) & (x['peak_location'] <= i + delta))
+            ok = ok or ((x['peak_location'] >= i - x['start'] - delta) & (x['peak_location'] <= i - x['start'] + delta))
         return ok
 
     def flag(self, x):
@@ -59,11 +60,8 @@ class TerminalCounter(AbstractCounter):
         
         low = int(10+x['peak_left'])
         high = int(10+x['peak_right']+1)
-        with open("src/output.txt", "w") as f: 
-            print(int(np.array(x.histogram)[low:high].sum()), file=f)
-            print(type(x.histogram), file=f)
         
-        return int(np.array(x.histogram)[low:high].sum())
+        return int(np.array(x['histogram'])[low:high].sum())
     
     def counts_byGroup(self, x):
         counts = []
@@ -108,6 +106,13 @@ class TerminalCounter(AbstractCounter):
         print(matrix)
 
     def collect_data(self):
+        if self.transcripts == {}:
+            self.df = pd.DataFrame({'chromosome':[], 'transcript_id':[], 'gene_id':[], 'prediction':[], 'counts':[], 'counts_byGroup':[], 'flag':[], 'group_id':[]})
+            if self.first:
+                self.df.to_csv(self.output_prefix, sep="\t", index=False, mode="w", header=True)
+            else:
+                self.df.to_csv(self.output_prefix, sep="\t", index=False, mode="a", header=False)
+            return
         self.df = pd.DataFrame({'transcript_id': self.transcripts.keys()})
         self.df['chromosome'] = self.df['transcript_id'].apply(lambda x: self.transcripts[x]['chr'])
         self.df['gene_id'] = self.df['transcript_id'].apply(lambda x: self.transcripts[x]['gene_id'])
@@ -159,15 +164,18 @@ class TerminalCounter(AbstractCounter):
         
         self.peaks['target'] = self.peaks['transcript_id'].apply(lambda x: [self.transcripts[x]['annotated']])
 
+        k = 0
         for id in self.transcripts:
             if "_" in id:
+                k += 1
                 if self.peaks['transcript_id'].eq(id.split("_")[0]).any():
                     self.peaks[self.peaks['transcript_id']==id.split("_")[0]]['target'] += [self.transcripts[id]['annotated']]
 
+        print(k)
 
         self.peaks['true_peak'] = self.peaks.apply(lambda x: self.peak_target(x) , axis = 1)
 
-        self.peaks = self.peaks.drop(['target', 'histogram'], axis = 1)
+        # self.peaks = self.peaks.drop('target', axis = 1)
 
         if self.first:
             self.peaks.to_csv("src/model_df.csv", index=False, header=True)
@@ -180,9 +188,12 @@ class TerminalCounter(AbstractCounter):
         self.model = XGBClassifier()
         self.model.load_model('src/model.json')
         params = self.model.get_xgb_params()
+        # {'booster': 'gbtree', 'learning_rate': 0.1711139976081603, 'gamma': 3.213024131696218, 'max_depth': 8, 'min_child_weight': 7.031066410000928, 'reg_lambda': 0.00035253812038082424, 'reg_alpha': 0.010639993402868728, 'subsample': 0.6310944088222122, 'colsample_bytree': 0.7856502544324856, 'n_estimators': 56, 'scale_pos_weight': 0.3971102549981997}
+        # {'booster': 'dart', 'learning_rate': 0.09115397231222125, 'gamma': 0.7911797085921515, 'max_depth': 6, 'min_child_weight': 8.890261356320382, 'reg_lambda': 3.4438281320977343e-07, 'reg_alpha': 4.583918034010515e-06, 'subsample': 0.9139332107255594, 'colsample_bytree': 0.9804235433943478, 'n_estimators': 95, 'scale_pos_weight': 0.41416529852133854}
+        params = {'booster': 'dart', 'learning_rate': 0.25989024187450366, 'gamma': 4.087502269121285, 'max_depth': 6, 'min_child_weight': 5.722951381412213, 'reg_lambda': 4.84758559371017, 'reg_alpha': 0.03795296107941127, 'subsample': 0.8809954546952385, 'colsample_bytree': 0.7483035972922923, 'n_estimators': 111, 'scale_pos_weight': 0.297061977156673}
         self.model_new = XGBClassifier(**params)
         
-        self.model_df = pd.read_csv("src/model_df.csv")
+        self.model_df = pd.read_csv("src/model_df.csv").drop('target', axis = 1)
 
         X = self.model_df.drop('true_peak', axis = 1).copy().astype(float, errors='ignore') 
         y = self.model_df['true_peak'].copy()
@@ -198,9 +209,9 @@ class TerminalCounter(AbstractCounter):
         X_test = X[X["chromosome"].isin(test_chrs)].drop('chromosome', axis = 1).copy().reset_index(drop=True)
         y_test = y[X["chromosome"].isin(test_chrs)].copy().reset_index(drop=True)
 
-        self.model_new.fit(X_train.drop(['peak_left', 'peak_right', 'annotated', 'rank', 'transcript_id', 'gene_id', 'start'], axis = 1).astype(float, errors='ignore'), y_train)
+        self.model_new.fit(X_train.drop(['peak_left', 'peak_right', 'histogram','annotated', 'rank', 'transcript_id', 'gene_id', 'start'], axis = 1).astype(float, errors='ignore'), y_train)
         
-        X_test['prediction'] = self.model_new.predict(X_test.drop(['peak_left', 'peak_right', 'annotated', 'rank', 'transcript_id', 'gene_id', 'start'], axis = 1).astype(float, errors='ignore'))
+        X_test['prediction'] = self.model_new.predict(X_test.drop(['peak_left', 'peak_right', 'histogram', 'annotated', 'rank', 'transcript_id', 'gene_id', 'start'], axis = 1).astype(float, errors='ignore'))
 
         X_test['true_peak'] = y_test
         self.result(X_test)
@@ -244,18 +255,27 @@ class TerminalCounter(AbstractCounter):
             self.df = self.df.drop(self.df[self.df.peak_count==0].index, axis = 0).reset_index(drop=True)
             self.dfResult['peak_heights'] = self.dfResult.apply(lambda x: x.histogram[x.prediction+10], axis = 1)
        
-            
+
         keys = list(self.df.peak_info[0].keys())
         self.peaks = self.df.drop('max', axis = 1).copy()
+
+
         for i in keys:
             self.peaks[i] = self.peaks.peak_info.apply(lambda x: x[i])
         self.peaks = self.peaks.drop(['left_thresholds', 'right_thresholds'], axis = 1)
         self.peaks = self.peaks.apply(lambda x: self.sort_peaks(x), axis = 1)
+
+
+
+        
+
+
         keys.remove('left_thresholds')
         keys.remove('right_thresholds')
         self.peaks = self.peaks.explode(keys+['peak_location', 'rank', 'peak_prominence', 'peak_width', 'peak_left', 'peak_right']).drop(['peak_info'], axis = 1).reset_index(drop=True)
              
-            
+
+
         self.peaks[['mean', 'var', 'range', 'skew', 'peak_count', 'peak_location', 'entropy', 
             'peak_heights', 'peak_width', 'peak_prominence',
             'rank']] = self.peaks[['mean', 'var', 'range', 'skew',
@@ -263,14 +283,39 @@ class TerminalCounter(AbstractCounter):
             'peak_heights', 'peak_width', 'peak_prominence', 'rank']].astype(float, errors='ignore') 
         
         
+
         self.peaks['prediction'] = self.model.predict(self.peaks.drop(['peak_left', 'peak_right', 'histogram', 
             'annotated', 'rank', 'transcript_id', 'gene_id', 'start', 'chromosome'], axis = 1).astype(float, errors='ignore'))
-    
-        self.peaks = self.peaks[self.peaks.prediction ==True].reset_index(drop=True)
+        
+
+        if self.first:
+            self.peaks.to_csv("src/peaks_1.csv", sep="\t", index=False, mode="w", header=True)
+        else:
+            self.peaks.to_csv("src/peaks_1.csv", sep="\t", index=False, mode="a", header=False)
+
+
+        self.peaks = self.peaks[self.peaks.prediction ==True].reset_index(drop=True)   
         self.peaks['prediction'] = self.peaks['peak_location']
+        
+
+
         self.dfResult = pd.concat([self.dfResult, self.peaks], axis=0).reset_index(drop=True)
         
+        
+        # if self.peaks.empty:
+        #     print("peaks is empty 6")
+
+        # if self.first:
+        #     self.dfResult.to_csv("src/dfResult.csv", sep="\t", index=False, mode="w", header=True)
+        # else:
+        #     self.dfResult.to_csv("src/dfResult.csv", sep="\t", index=False, mode="a", header=False)
+
+
         self.dfResult['counts'] = self.dfResult.apply(lambda x: self.counts(x), axis = 1)
+
+        
+
+
         self.dfResult['prediction'] += self.dfResult['start']
         self.dfResult['prediction'] = self.dfResult['prediction'].astype(int)
         self.dfResult['flag'] = self.dfResult.apply(lambda x: self.flag(x), axis = 1)
@@ -296,6 +341,31 @@ class TerminalCounter(AbstractCounter):
             self.dfResult.to_csv(self.output_prefix, sep="\t", index=False, mode="w", header=True)
         else:
             self.dfResult.to_csv(self.output_prefix, sep="\t", index=False, mode="a", header=False)
+
+    def test_model(self):
+        self.model = XGBClassifier()
+        self.model.load_model('src/model.json')
+        params = self.model.get_xgb_params()
+        params = {'booster': 'dart', 'learning_rate': 0.09115397231222125, 'gamma': 0.7911797085921515, 'max_depth': 6, 'min_child_weight': 8.890261356320382, 'reg_lambda': 3.4438281320977343e-07, 'reg_alpha': 4.583918034010515e-06, 'subsample': 0.9139332107255594, 'colsample_bytree': 0.9804235433943478, 'n_estimators': 95, 'scale_pos_weight': 0.41416529852133854}
+        self.model_new = XGBClassifier(**params)
+
+        self.model_df = pd.read_csv("src/model_df.csv")
+
+        X = self.model_df.drop('true_peak', axis = 1).copy().astype(float, errors='ignore') 
+        y = self.model_df['true_peak'].copy()
+        
+        chrs = list(X.chromosome.unique())
+        n = len(chrs)//2
+        test_chrs = random.sample(chrs, n)
+
+        X_test = X[X["chromosome"].isin(test_chrs)].drop('chromosome', axis = 1).copy().reset_index(drop=True)
+        y_test = y[X["chromosome"].isin(test_chrs)].copy().reset_index(drop=True)
+
+        
+        X_test['prediction'] = self.model.predict(X_test.drop(['peak_left', 'peak_right', 'annotated', 'rank', 'transcript_id', 'gene_id', 'start'], axis = 1).astype(float, errors='ignore'))
+
+        X_test['true_peak'] = y_test
+        self.result(X_test)
         
 
 
@@ -340,9 +410,9 @@ class TSSCounter(TerminalCounter):
         
     def dump(self):
         self.model = XGBClassifier()
-        self.model.load_model('src/model_new.json')
+        self.model.load_model('src/model.json')
 
-        # self.create_df()
+        self.create_df()
         # self.collect_data()
 
         self.transcripts = {}
@@ -362,8 +432,6 @@ class PolyACounter(TerminalCounter):
             self.gene_info: GeneInfo = read_assignment.gene_info
         group_id = read_assignment.read_group
         group_id = self.group_numeric_ids[group_id]
-        if read_assignment.assignment_type != ReadAssignmentType.intergenic:
-            print("1")
 
         # add a single read_assignment to features dataframe
         if read_assignment.polyA_found == True and read_assignment.assignment_type in [ReadAssignmentType.inconsistent_non_intronic, ReadAssignmentType.unique, ReadAssignmentType.unique_minor_difference, ReadAssignmentType.inconsistent]:
@@ -393,14 +461,18 @@ class PolyACounter(TerminalCounter):
      
     def dump(self):
         self.model = XGBClassifier()
-        self.model.load_model('src/model.json')
+        # self.model.load_model('src/model.json')
+        self.model.load_model('src/model_new.json')
 
-        self.create_df()
+
+        # self.create_df()
+        # self.collect_data()
 
         self.transcripts = {}
         self.first = False
 
     def finalize(self, args=None):
+        # self.test_model()
         # self.train_model()
         pass
 
