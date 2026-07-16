4c7718985c9f7b4578585b7a302388088e4a83cc
Andrey Prjibelski <andrewprzh@gmail.com>
2026-07-16 20:41:49 +0300

    create visualizer subpackage

---
+++ isoquant_lib/visualizer/__init__.py
@@ -0,0 +1,8 @@
+############################################################################
+# Copyright (c) 2022-2026 University of Helsinki
+# # All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
+# Post-run visualization: output config, plotting, and gene-model ranking
+# used by isoquant_visualize.py.
